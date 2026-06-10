f171e6dc1757a87da42ed2ef35e5f770d0ab05c8
Joshua Cortez <joshua.m.cortez@gmail.com>
2026-06-11 02:42:07 +0800

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        start_symbols = set(["(", "{", "["])
+
+        reverse_mapping = {
+            ")":"(",
+            "}":"{",
+            "]":"[",
+        }
+            
+        stack = []
+        for char in s:
+            if char in start_symbols:
+                stack.append(char)
+            if char in reverse_mapping.keys():
+                if not stack:
+                    return False
+                else: 
+                    if stack[-1] == reverse_mapping[char]:
+                        stack.pop()
+                    else:
+                        return False
+        
+        return not stack
