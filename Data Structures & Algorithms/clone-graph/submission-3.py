26c772413de38000d440f443360c4dffb96f7d6c
Joshua Cortez <joshua.m.cortez@gmail.com>
2026-06-15 19:59:27 +0800

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # BFS outline
+        # 1. setup
+        # 2. add to queue
+        # 3. add boundary conditions
+        # 4. add neighbors
+
+        if not node:
+            return None
+
+        queue = deque()
+        queue.append(node)
+
+        visited = {node:Node(node.val)} 
+
+        while queue:
+            for i in range(len(queue)):
+                curr_node = queue.popleft()
+
+                if curr_node not in visited:  
+                    visited[curr_node] = Node(curr_node.val)
+
+                for neighbor_node in curr_node.neighbors:
+                    if neighbor_node not in visited:
+                        neighbor_clone = Node(neighbor_node.val)
+                        visited[neighbor_node] = neighbor_clone
+                        queue.append(neighbor_node)
+                    else:
+                        neighbor_clone = visited[neighbor_node]
+                    visited[curr_node].neighbors.append(neighbor_clone)
+                        
+        return visited[node]
+        
