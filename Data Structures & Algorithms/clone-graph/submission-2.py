8fcb2adc777a1db70eabbc81870c8e94897c8041
Joshua Cortez <joshua.m.cortez@gmail.com>
2026-06-15 19:13:57 +0800

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # DFS outline
+        # 1. setup
+        # 2. boundary conditions
+        # 3. DFS to neighbors
+        # 4. backtracking if needed
+
+        visited_nodes = {}
+
+        def dfs(node):
+            nonlocal visited_nodes
+
+            if not node:
+                return None
+
+            if node.val in visited_nodes:
+                return visited_nodes[node.val]
+                
+            cloned_node = Node(node.val,[])
+            visited_nodes[node.val] = cloned_node
+         
+            for neighbor in node.neighbors:
+                neighbor_clone = dfs(neighbor)
+                cloned_node.neighbors.append(neighbor_clone)
+                
+            return cloned_node
+    
+        clone = dfs(node)
+        
+        return clone
+
+
