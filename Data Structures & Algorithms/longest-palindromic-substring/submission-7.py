8b2bfb5c4eddf9f16b61726abaae8462164b9d89
Joshua Cortez <joshua.m.cortez@gmail.com>
2026-06-16 13:43:33 +0800

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+        def expanding_twopointers(s, left, right) -> str:
+            """
+            Returns the longest palindrome found
+            """
+            found_palindrome = ""
+            while left >= 0 and right <= len(s) - 1:
+                if s[left] != s[right]:
+                    break
+                else:
+                    found_palindrome = s[left:right+1]
+                left-=1
+                right+=1
+
+            return found_palindrome
+
+        best_palindrome = ""
+        # odd case
+        for i in range(len(s)-1):
+            left = i
+            right = i
+            found_palindrome = expanding_twopointers(s, left, right)
+            if len(found_palindrome) > len(best_palindrome):
+                best_palindrome = found_palindrome
+        
+        # even case
+        for i in range(len(s)-1):
+            left = i
+            right = i+1
+            found_palindrome = expanding_twopointers(s, left, right)
+            if len(found_palindrome) > len(best_palindrome):
+                best_palindrome = found_palindrome
+
+        return best_palindrome
+
+        
