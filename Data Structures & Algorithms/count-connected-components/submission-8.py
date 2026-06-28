59d9c1090c2c9b1626564d3b6e9ed7f45d071ebd
Joshua Cortez <joshua.m.cortez@gmail.com>
2026-06-28 21:00:09 +0800

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.py
@@ -0,0 +1,58 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # unionfind approach
+        # find -> finding the parent node
+        # union -> joining two trees together
+
+        class UnionFind:
+            def __init__(self, n):
+                self.parent = {}
+                self.rank = {}
+
+                for i in range(n):
+                    self.parent[i] = i
+                    self.rank[i] = 0
+
+            def find(self, n):
+                p = n
+                while p != self.parent[p]:
+                    # memoization step
+                    self.parent[p] = self.parent[self.parent[p]]
+                    # go up a level
+                    p = self.parent[p]
+                
+                return p
+
+            def union(self, n1, n2) -> bool:
+                """
+                Try taking the union if different trees
+                But if same tree just skip
+                """
+                p1 = self.find(n1)
+                p2 = self.find(n2)
+
+                # don't union they're actually the same tree
+                # i.e. same root
+                if p1 == p2:
+                    return False
+
+                if self.rank[p1] > self.rank[p2]:
+                    self.parent[p2] = p1
+                elif self.rank[p2] > self.rank[p1]:
+                    self.parent[p1] = p2
+                else:
+                    self.parent[p1] = p2
+                    self.rank[p2] += 1
+
+                return True
+
+        unionfind = UnionFind(n)
+        n_components = n
+        for (node_a, node_b) in edges:
+            made_union = unionfind.union(node_a, node_b)
+            if made_union:
+                n_components -= 1
+
+        return n_components
+
+        
